22d452219926684af9e91f953e3ad3bbba924279
iamatharvak <kulkarni.atharva.1403@gmail.com>
2025-01-23 01:23:21 +0530

    half login

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,6 +1,55 @@
 import React from "react";
+import { Box, Container, Text, Tabs } from "@chakra-ui/react";
+import Login from "../UIComponents/Authentication/Login";
+import Signup from "../UIComponents/Authentication/Signup";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  const items = [
+    {
+      title: "Login",
+      content: <Login />,
+    },
+    {
+      title: "Sign Up",
+      content: <Signup />,
+    },
+  ];
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0px 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work Sans">
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs.Root defaultValue="1" width="full">
+          <Tabs.List mb="1rem">
+            {items.map((item, index) => (
+              <Tabs.Trigger key={index} value={item.title} width="50%">
+                Tab {item.title}
+              </Tabs.Trigger>
+            ))}
+          </Tabs.List>
+          <Box width="100%" color="black">
+            {items.map((item, index) => (
+              <Tabs.Content key={index} value={item.title}>
+                {item.content}
+              </Tabs.Content>
+            ))}
+          </Box>
+        </Tabs.Root>
+      </Box>
+    </Container>
+  );
 };
 export default Homepage;
---
+++ frontend/src/UIComponents/Authentication/Login.jsx
@@ -0,0 +1,10 @@
+import { VStack } from "@chakra-ui/react";
+import React from "react";
+
+const Login = () => {
+  return <VStack borderSpacing="5px">
+    
+  </VStack>;
+};
+
+export default Login;
