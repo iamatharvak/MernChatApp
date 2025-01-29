d242cca4f2fde442cafcf3df3f0c75a756ac31d5
iamatharvak <kulkarni.atharva.1403@gmail.com>
2025-01-29 16:36:35 +0530

    db added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const colors = require("colors");
 const { chats } = require("./data/data");
+const connectDB = require("./data/config/db");
+dotenv.config();
+connectDB();
 const app = express();
 app.use(cors());
-dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Api started running");
@@ -20,4 +23,7 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT = process.env.Port || 5000;
 
-app.listen(PORT, console.log(`Server Started at Port number ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server Started at Port number ${PORT}`.yellow.bold)
+);
