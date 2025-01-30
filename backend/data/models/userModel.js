8b6fc706fce4def9e57711ee865bcf28fa7e7a9c
iamatharvak <kulkarni.atharva.1403@gmail.com>
2025-01-30 12:43:57 +0530

    authentication

--- backend/data/models/userModel.js
+++ backend/data/models/userModel.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
+const bycrpt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     pic: {
       type: String,
-      required: true,
       default: "https://img.icons8.com/skeuomorphism/64/skull.png",
     },
   },
@@ -15,6 +15,19 @@ const userSchema = mongoose.Schema(
     timeStamps: true,
   }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bycrpt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bycrpt.genSalt(10);
+  this.password = bycrpt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,32 @@ const cors = require("cors");
 const colors = require("colors");
 const { chats } = require("./data/data");
 const connectDB = require("./data/config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
 dotenv.config();
 connectDB();
+
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Api started running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-app.get("/api/chat/:id", (req, res) => {
-  const singlechat = chats.find((chat) => chat._id === req.params.id);
-  res.send(singlechat);
-});
+app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+// app.get("/api/chat", (req, res) => {
+//   res.send(chats);
+// });
+// app.get("/api/chat/:id", (req, res) => {
+//   const singlechat = chats.find((chat) => chat._id === req.params.id);
+//   res.send(singlechat);
+// });
 
 const PORT = process.env.Port || 5000;
 
