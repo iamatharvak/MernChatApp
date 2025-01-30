17eba7260b460d57690834b28e577186b6788e62
iamatharvak <kulkarni.atharva.1403@gmail.com>
2025-01-30 23:16:37 +0530

    api integrated

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const colors = require("colors");
 const { chats } = require("./data/data");
 const connectDB = require("./data/config/db");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatroutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
